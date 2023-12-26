5df4cc4230cc462f243a5ecfc41556f207b5bdac
David Marom <davidmarom.pro@gmail.com>
2023-12-26 09:53:26 +0200

    wip

--- src/components/RecCard/RecCard.jsx
+++ src/components/RecCard/RecCard.jsx
@@ -6,11 +6,16 @@ import { Image } from 'next/image';
 
 export default function RecCard({ data }) {
     const { strMeal, strMealThumb, strCategory, strInstructions, idMeal } = data;
+
     return (
         <Container>
-            <img src={strMealThumb} alt={strMeal} />
+            <img src={strMealThumb || ''} alt={strMeal} />
             <InfoContainer>
+                <div className="row-between">
                 <h3>{strMeal}</h3>
+                <div className='col-start'><img src="/icons/star-full.svg" alt="star" width={20} height={20} /></div>
+
+                </div>
                 <h4>{strCategory}</h4>
                 <p>{strInstructions}</p>
                 <button onClick={() => readMore(idMeal)}>Read More</button>
@@ -18,6 +23,7 @@ export default function RecCard({ data }) {
 
         </Container>
     )
+
 
 }
 
