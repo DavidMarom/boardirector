a77feb25ba21f7893f7742bbf37c5b5171a727e9
David Marom <davidmarom.pro@gmail.com>
2023-12-27 08:04:32 +0200

    wip

--- src/store/recipes.js
+++ src/store/recipes.js
@@ -1,14 +1,14 @@
 import { create } from "zustand";
 
 const useRecipesStore = create((set) => ({
-    recipes: ['aaa'],
+    recipes: [],
     setRecipes: () => set((state) => ({ recipes: state })),
 
     selectedCategory: "",
-    setSelectedCategory: (category) => set((state) => ({ selectedCategory: category })),
+    setSelectedCategory: (category) => set(() => ({ selectedCategory: category })),
     
     categories: [],
-    setCategories: (categories) => set((state) => ({ categories })),
+    setCategories: (categories) => set(() => ({ categories })),
     
     favorites: [],
     setFavorites: (favorites) => set(() => ({ favorites })),
